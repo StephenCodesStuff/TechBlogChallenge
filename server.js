e82b023c3a7e8bada0524931f87213fe156a63dc
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-06-28 20:04:02 -0500

    working on login

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const exhbs = require('express-handlebars');
+const exhbs = require('express-handlebars');;
 const path =require('path');
 const routes = require('./controllers');
 const hbs = exhbs.create();
