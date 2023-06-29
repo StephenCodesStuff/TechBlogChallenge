80b7f6156e78daba40096f7f32ab9e8d4f6733e3
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-06-28 21:28:00 -0500

    added log out and log in functions

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exhbs = require('express-handlebars');;
-const path =require('path');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const hbs = exhbs.create();
+const helpers = require('./util/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -28,8 +31,9 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine("handlebars",hbs.engine);
-app.set("view engine", "handlebars")
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -38,5 +42,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening http://localhost:${PORT}/`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
