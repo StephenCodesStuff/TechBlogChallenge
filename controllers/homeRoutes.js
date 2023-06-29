26b31fdaf30f2c00e4ab91aaa7403dac6ca818f4
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-06-28 19:16:30 -0500

    adding handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,7 +24,59 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
   });
+  
+  router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+      console.log(userData)
+      
 
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // router.get('/editpost/:id', async (req, res) => {
+  //   try {
+  //     const postData = await Post.findByPk(req.params.id, {
+  //       include: [
+  //         {
+  //           model: User,
+  //           attributes: [''],
+  //         },
+  //       ],
+  //     });
+  
+  //     const blog = postData.get({ plain: true });
+  
+  //     res.render('editpost', {
+  //       ...blog,
+  //       logged_in: req.session.logged_in
+  //     });
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // });
+
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('login');
+  });
 
   module.exports = router;
 
